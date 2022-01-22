c1875a935e3299a7fda58dd8a315f3a065585ab2
John Toriola <john.toriola.tj@gmail.com>
2022-01-22 10:32:46 +0100

    updated header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { BsCart2 } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   return (
     <section className="header">
-      <h1 className="header__logo">
-        B<span>AT</span>A
-      </h1>
-      <div className="header__cart-container">
-        <BsCart2 className="header__cart" />
-        <p className="header__cart-text">My Cart</p>
+      <div className="header__start">
+        <FaBars className="header__menu" />
+        <h1 className="header__logo">BATA</h1>
       </div>
+      <BsCart2 className="header__cart" />
     </section>
   );
 };
