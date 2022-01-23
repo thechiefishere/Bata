87657c09a4ac1dcce4e3810c0bf6bcf585b9a706
John Toriola <john.toriola.tj@gmail.com>
2022-01-23 17:55:37 +0100

    created product details and image slider

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoadingState } from "../store/actions";
+import Loading from "./Loading";
+import ImageSlider from "./ImageSlider";
+
+const ProductDetail = () => {
+  const [product, setProduct] = useState(null);
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.isLoadingData);
+
+  const fetchProduct = async () => {
+    dispatch(setLoadingState(true));
+    const response = await fetch(
+      `http://localhost:5000/api/v1/footwears/${productId}`
+    );
+    const data = await response.json();
+    if (data.status === "success") {
+      dispatch(setLoadingState(false));
+      setProduct(data.item);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      {product !== null && (
+        <section>
+          <ImageSlider gallery={product.image} />
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default ProductDetail;
