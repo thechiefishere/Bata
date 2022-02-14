83de1ae12ba8fa1f3b6a05a3350c20d6e77e537d
John Toriola <john.toriola.tj@gmail.com>
2022-02-14 07:56:11 +0100

    updated loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Loading.css";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return <div className="loading">Loading...</div>;
 };
 
 export default Loading;
